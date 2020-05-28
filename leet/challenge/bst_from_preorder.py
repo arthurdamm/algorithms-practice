fadd384646c280c371d6c4ef30b5432b97e80a24
Arthur Damm <indigoarthur@gmail.com>
2020-05-28 23:44:53 +0000

    add bst_from_preorder.py

---
+++ leet/challenge/bst_from_preorder.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Construct Binary Search Tree from Preorder Traversal
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        self.i = 1
+        def insert(node, hi):
+            if self.i < len(preorder) and preorder[self.i] < node.val:
+                node.left = TreeNode(preorder[self.i])
+                self.i += 1
+                insert(node.left, node.val)
+            if self.i < len(preorder) and preorder[self.i] <= hi:
+                node.right = TreeNode(preorder[self.i])
+                self.i += 1
+                insert(node.right, hi)
+
+        root = TreeNode(preorder[0])
+        insert(root, float('inf'))
+        return root

5def9c60a7bec77592c16a69c0f15c3a643402b5
Arthur Damm <indigoarthur@gmail.com>
2020-08-09 19:45:48 -0700

    Use queue canonically and pythonic inequality

--- leet/rotting_oranges.py
+++ leet/rotting_oranges.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from collections import deque
 
 class Solution:
     def orangesRotting(self, grid):
-        rotting_tuples = []
+        rotting_tuples = deque()
         fresh_count = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
@@ -17,26 +18,22 @@ class Solution:
         minutes = 0
         
         def rot(i, j):
-            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[i]) or grid[i][j] != 1:
-                return 0
-            new_rotting_tuples.append((i, j))
-            grid[i][j] = 2
-            return 1
+            if 0<=i<len(grid) and 0<=j<len(grid[i]) and grid[i][j] == 1:
+                rotting_tuples.append((i, j))
+                grid[i][j] = 2
+                return 1
+            return 0
         
-        while fresh_count > 0:
+        while fresh_count and len(rotting_tuples) > 0:
             minutes += 1
-            new_rotting_tuples = []
-            for rotting in rotting_tuples:
+            for _ in range(len(rotting_tuples)):
+                rotting = rotting_tuples.popleft()
                 fresh_count -= (rot(rotting[0] - 1, rotting[1]) +
                 rot(rotting[0], rotting[1] - 1) +
                 rot(rotting[0] + 1, rotting[1]) +
                 rot(rotting[0], rotting[1] + 1))
-            if not new_rotting_tuples:
-                minutes = -1
-                break
-            rotting_tuples = new_rotting_tuples
-            new_rotting_tuples = []
-        return minutes
+        return minutes if not fresh_count else -1
 
 input = [[2,1,1],[1,1,0],[0,1,1]]
+#input = [[2,1,1],[0,1,1],[1,0,1]]
 print("Solution to", input, Solution().orangesRotting(input))

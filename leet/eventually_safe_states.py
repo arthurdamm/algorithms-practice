d8b05989f39dc5fa5b30bdbc3d88ed4ec01de39c
Arthur Damm <indigoarthur@gmail.com>
2020-08-11 19:30:07 +0000

    Use CONSTANTS for clarity

--- leet/eventually_safe_states.py
+++ leet/eventually_safe_states.py
@@ -2,19 +2,19 @@
 
 class Solution:
     def eventualSafeNodes(self, graph) -> int:
-        visited = {}
+        VISITED, SAFE, visited = 1, 2, {}
         def dfs(node):
             if node in visited:
                 return visited[node]
-            visited[node] = 1
-            if all([dfs(neighbor) == 2 for neighbor in graph[node]]):
-                visited[node] = 2
-                return 2
-            return 1
+            visited[node] = VISITED
+            if all([dfs(neighbor) == SAFE for neighbor in graph[node]]):
+                visited[node] = SAFE
+                return SAFE
+            return VISITED
         for node in range(len(graph)):
             if node not in visited:
                 dfs(node)
-        return sorted([node for node in visited.keys() if visited[node] == 2])
+        return sorted([node for node in visited.keys() if visited[node] == SAFE])
 
 graph = [[1,2],[2,3],[5],[0],[5],[],[]]
 print("Solution to", graph, Solution().eventualSafeNodes(graph))

d4e11ed71603799e8c695a4a1ad4b492d5436f86
Arthur Damm <indigoarthur@gmail.com>
2020-08-23 23:56:51 +0000

    add path_sum_3_slow.py

---
+++ leet/path_sum_3/path_sum_3_slow.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.counter = 0
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        def dfs(node, sum):
+            if not node.left and not node.right:
+                if (node.val == sum):
+                    self.counter += 1
+                return
+            if node.val == sum:
+                self.counter += 1
+            if node.left:
+                dfs(node.left, sum - node.val)
+            if node.right:
+                dfs(node.right, sum - node.val)
+
+        if not root:
+            return self.counter
+        dfs(root, sum)
+        if root.left:
+            self.pathSum(root.left, sum)
+        if root.right:
+            self.pathSum(root.right, sum)
+        return self.counter
+

c58b675c4b67af7a01657823c83edaa6a6903fe6
Arthur Damm <indigoarthur@gmail.com>
2020-09-09 16:27:31 +0000

    add 165_compare_version_numbers.py

---
+++ leet/165_compare_version_numbers.py
@@ -0,0 +1,11 @@
+# LeetCode 165. Compare Version Numbers
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1 = version1.split(".")
+        v2 = version2.split(".")
+        for i in range(max(len(v1), len(v2))):
+            a = int(v1[i]) if i < len(v1) else 0
+            b = int(v2[i]) if i < len(v2) else 0
+            if a != b: return (a - b) // abs(a - b)
+        return 0
+

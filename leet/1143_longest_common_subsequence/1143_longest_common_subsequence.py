d16815c666dde453ae2c5d22aba67f3418a4c04a
Arthur Damm <indigoarthur@gmail.com>
2020-09-12 21:11:48 -0700

    Add LeetCode #1143. Longest Common Subsequence

---
+++ leet/1143_longest_common_subsequence/1143_longest_common_subsequence.py
@@ -0,0 +1,13 @@
+# LeetCode # 1143. Longest Common Subsequence
+# O(n * m) time-and-space
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text1) + 1) for _ in range(len(text2) + 1)]
+        for i in range(1, len(text2) + 1):
+            for j in range(1, len(text1) + 1):
+                if text2[i - 1] == text1[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[-1][-1]
+

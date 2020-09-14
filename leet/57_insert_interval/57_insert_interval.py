2f91ac12e2fd94b43879d3b101132c7b7facc067
Arthur Damm <indigoarthur@gmail.com>
2020-09-13 21:00:52 -0700

    Remove redundant logic

--- leet/57_insert_interval/57_insert_interval.py
+++ leet/57_insert_interval/57_insert_interval.py
@@ -13,23 +13,19 @@ class Solution:
                 # if start-time is > than new start, change start time
                 if intervals[i][0] > newInterval[0]:
                     intervals[i][0] = newInterval[0]
-                # if end-time is >= new end, 1 merge and quit
-                if intervals[i][1] >= newInterval[1]:
-                    return intervals
-                # else keep iterating until finding such or end of array
-                for j in range(i + 1, len(intervals)):
+                # iterate from i until finding end-time is >= new end
+                for j in range(i, len(intervals)):
                     if intervals[j][1] >= newInterval[1]:
                         # if start time is > new end, merge up to last
                         if intervals[j][0] > newInterval[1]:
                             # set ith end-time to new end time
                             intervals[i][1] = newInterval[1]
-                            if j > i + 1:
-                                intervals[i + 1: j] = []
+                            intervals[i + 1: j] = []
                             return intervals
                         else:
                             # set end time of i'th interval to b's end time
                             intervals[i][1] = intervals[j][1]
-                            intervals[i + 1: j + 1] = []    
+                            intervals[i + 1: j + 1] = []
                             return intervals
                         return
                 # new end greater than any other interval
@@ -40,6 +36,7 @@ class Solution:
         # new interval is largest append to end
         intervals += [newInterval]
         return intervals
+
 if __name__ == "__main__":
   intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
   newInterval = [4,8]

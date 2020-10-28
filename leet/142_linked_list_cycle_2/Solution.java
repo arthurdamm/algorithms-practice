920d18db1920f040491960600a58d89ae96dfee6
Arthur Damm <indigoarthur@gmail.com>
2020-10-27 18:05:11 -0700

    Add 142. Linked List Cycle II

---
+++ leet/142_linked_list_cycle_2/Solution.java
@@ -0,0 +1,35 @@
+// 142. Linked List Cycle II
+// O(n) time, O(1) space
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                break;
+        }
+        if (fast == null || fast.next == null)
+            return null;
+        slow = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+}
+

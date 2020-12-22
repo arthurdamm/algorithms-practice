854b09f88c0a4069fa48705f7a4ca3771f7b99d1
Arthur Damm <indigoarthur@gmail.com>
2020-12-22 09:08:59 -0800

    Implement buckets for #895

--- leet/0895_maximum_frequency_stack/SolutionB.java
+++ leet/0895_maximum_frequency_stack/SolutionB.java
@@ -1,30 +1,28 @@
 // LeetCode 895. Maximum Frequency Stack
-// push() & pop() O(logn) time, O(n) space
-class Tuple {
-    int n;
-    int k;
-    int i;
-    Tuple(int n, int k, int i) { this.n = n; this.k = k; this.i = i; }
-}
-
+// push() & pop() O(1) time, O(n) space
 class FreqStack {
-    int i = 0;
-    HashMap<Integer, Integer> map = new HashMap<>();
-    PriorityQueue<Tuple> pq = new PriorityQueue<>(
-        (t1, t2) -> t2.k == t1.k ? t2.i - t1.i : t2.k - t1.k);
-    
+    int max = 0;
+    Map<Integer, Integer> counts = new HashMap<>();
+    Map<Integer, Stack<Integer>> buckets = new HashMap<>();
+
     public FreqStack() {
+        
     }
     
     public void push(int x) {
-        int k = map.getOrDefault(x, 0) + 1;
-        map.put(x, k);
-        pq.add(new Tuple(x, k, i++));
+        int k = counts.getOrDefault(x, 0) + 1;
+        counts.put(x, k);
+        max = Math.max(max, k);
+        if (!buckets.containsKey(k))
+            buckets.put(k, new Stack<>());
+        buckets.get(k).push(x);
     }
     
     public int pop() {
-        int n = pq.poll().n;
-        map.put(n, map.get(n) - 1);
-        return n;
+        int x = buckets.get(max).pop();
+        if (buckets.get(max).empty())
+            max--;
+        counts.put(x, counts.get(x) - 1);
+        return x;
     }
 }

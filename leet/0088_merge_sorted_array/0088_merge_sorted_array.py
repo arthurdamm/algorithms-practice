5e203b8996c6a7b6fb315157f88c7121ad89a61b
Arthur Damm <indigoarthur@gmail.com>
2021-01-11 09:02:59 -0800

    Add LeetCode 88. Merge Sorted Array

---
+++ leet/0088_merge_sorted_array/0088_merge_sorted_array.py
@@ -0,0 +1,18 @@
+# LeetCode 88. Merge Sorted Array
+# https://leetcode.com/problems/merge-sorted-array/
+# O(m + n) time, O(1) space
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        m, n, k = m - 1, n - 1, len(nums1) - 1
+        while m >= 0 and n >= 0:
+            if nums1[m] >= nums2[n]:
+                nums1[k] = nums1[m]
+                m -= 1
+            else:
+                nums1[k] = nums2[n]
+                n -= 1
+            k -= 1
+        while n >= 0:
+            nums1[k] = nums2[n]
+            k -= 1
+            n -= 1
---
+++ leet/0088_merge_sorted_array/Solution.java
@@ -0,0 +1,18 @@
+// LeetCode 88. Merge Sorted Array
+// https://leetcode.com/problems/merge-sorted-array/
+// O(m + n) time, O(1) space
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = nums1.length - 1;
+        m--;
+        n--;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] >= nums2[n])
+                nums1[k--] = nums1[m--];
+            else
+                nums1[k--] = nums2[n--];
+        }
+        while (n >= 0)
+            nums1[k--] = nums2[n--];
+    }
+}

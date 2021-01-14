9384f08b563c334e4d686f73a7e027e0605e7e33
Arthur Damm <indigoarthur@gmail.com>
2021-01-14 13:28:19 -0800

    Add prefix sum solution to #1658

--- leet/1658_min_operations_reduce_x_zero/1658_min_operations_reduce_x_zero.py
+++ leet/1658_min_operations_reduce_x_zero/1658_min_operations_reduce_x_zero.py
@@ -14,3 +14,27 @@ class Solution:
             if target == 0:
                 maxSubarray = max(maxSubarray, j - i + 1)
         return len(nums) - maxSubarray if maxSubarray > 0 else -1
+
+# O(n) time O(n) space using a Prefix Sum dictionary
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        leftSums, currSum, minOps = defaultdict(int), 0, float('inf')
+        for i in range(len(nums)):
+            currSum += nums[i]
+            leftSums[currSum] = i + 1
+            if currSum > x:
+                break
+        if currSum < x:
+            return -1
+        if x in leftSums:
+            minOps = leftSums[x]
+        currSum = 0
+        for i in range(len(nums) - 1, -1, -1):
+            currSum += nums[i]
+            if currSum == x:
+                minOps = min(minOps, len(nums) - i)
+            elif x - currSum in leftSums:
+                minOps = min(minOps, leftSums[x - currSum] + len(nums) - i)
+            elif currSum > x:
+                break
+        return -1 if minOps == float('inf') else minOps

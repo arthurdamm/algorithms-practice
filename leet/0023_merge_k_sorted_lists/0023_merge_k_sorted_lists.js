3527dff0caf0c6a90f66ae2b5cdc03f761c6c891
Arthur Damm <indigoarthur@gmail.com>
2021-01-24 16:48:46 -0800

    Tighten up 0023.js

--- leet/0023_merge_k_sorted_lists/0023_merge_k_sorted_lists.js
+++ leet/0023_merge_k_sorted_lists/0023_merge_k_sorted_lists.js
@@ -2,19 +2,16 @@
 // https://leetcode.com/problems/merge-k-sorted-lists/
 // O(nlogk) time, O(logk) space
 var mergeKLists = function(lists) {
-    
     let divideAndConquerLists = function(lists, start, end) {
-        if (start >= end)
-            return lists[start];
+        if (start == end) return lists[start];
         let mid = parseInt(start + (end - start) / 2);
         let left = divideAndConquerLists(lists, start, mid);
         let right = divideAndConquerLists(lists, mid + 1, end);
         return mergeTwoLists(left, right);
     }
-    
     let mergeTwoLists = function(list1, list2) {
         let dummy = new ListNode(), head = dummy;
-        while (list1 && list2) {
+        for (; list1 && list2; head = head.next) {
             if (list1.val <= list2.val) {
                 head.next = list1;
                 list1 = list1.next;
@@ -22,16 +19,10 @@ var mergeKLists = function(lists) {
                 head.next = list2;
                 list2 = list2.next;
             }
-            head = head.next;
         }
-        if (list1)
-            head.next = list1;
-        if (list2)
-            head.next = list2;
+        if (list1) head.next = list1;
+        if (list2) head.next = list2;
         return dummy.next;
     }
-    
-    if (!lists.length)
-        return null;
-    return divideAndConquerLists(lists, 0, lists.length);
+    return lists.length ? divideAndConquerLists(lists, 0, lists.length) : null;
 };

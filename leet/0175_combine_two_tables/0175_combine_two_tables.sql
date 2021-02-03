2ec9c8139a7908154de282fe5aa74a365b0835c2
Arthur Damm <indigoarthur@gmail.com>
2021-02-02 19:46:16 -0800

    Add LeetCode 175. Combine Two Tables

---
+++ leet/0175_combine_two_tables/0175_combine_two_tables.sql
@@ -0,0 +1,6 @@
+# LeetCode 175. Combine Two Tables
+# https://leetcode.com/problems/combine-two-tables/
+SELECT FirstName, LastName, City, State
+FROM Person
+LEFT JOIN Address
+ON Person.PersonId = Address.PersonId;

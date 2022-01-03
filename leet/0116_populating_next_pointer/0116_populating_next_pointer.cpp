caf07bede270f30273aa3b7f6c6f50a63c6e89f0
Arthur Damm <indigoarthur@gmail.com>
2022-01-02 17:49:25 -0800

    add 0116_populating_next_pointer.cpp

---
+++ leet/0116_populating_next_pointer/0116_populating_next_pointer.cpp
@@ -0,0 +1,16 @@
+// LeetCode 116. Populating Next Right Pointers in Each Node
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+// O(n) time, O(1) space
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root || !(root->left))
+            return root;
+        root->left->next = root->right;
+        if (root->next)
+            root->right->next = root->next->left;
+        connect(root->left);
+        connect(root->right);
+        return root;
+    }
+};
